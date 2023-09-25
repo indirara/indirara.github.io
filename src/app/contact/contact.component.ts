08fbfde07486b37bd28c4388a4d77df186157ed3
indirara <indiraprofir@gmail.com>
2023-09-26 00:15:31 +0100

    made it a bit more responsive

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class ContactComponent {
 
+  constructor(private readonly httpClient: HttpClient) {}
+
+  getInstaFeed(): void {
+    const apiVersion = '11.0';
+    const userId = 'indira.vtm';
+  }
 }
